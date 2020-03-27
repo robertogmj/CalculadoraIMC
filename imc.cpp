ad917fbd8c96a8786a67cdcf69f4384887254ec5
rgmj <robertogmj@gmail.com>
2020-03-26 23:01:48 -0300

    Criacao do Primeiro Arquivo no Repositorio

---
+++ imc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ //declaracao variaveis
+    int kg;
+    double a,cimc;
+
+    cout << "Entre com o seu peso em quilos? ";
+    cin >> kg;
+
+    cout << "Entre com a sua altura em metros e centimetros(1.80) ";
+    cin >> a;
+
+    cimc = kg / (a * a);
+
+    cout << "O seu IMC é de: " << cimc;
+
+}
